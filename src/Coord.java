2010a017117206a49b19668ce9c674ba4fa7b9b8
Qiwen Zhu <qiwzhu@indiana.edu>
2017-02-01 22:17:24 -0500

    hashCode(), onBoard(), and neighbors() finished

--- src/Coord.java
+++ src/Coord.java
@@ -7,159 +7,203 @@ import java.util.List;
  * to right. Y-coordinates increase from top to bottom. Operations are provided
  * to easily locate neighboring coordinates (in the NSEW compass directions)
  * on a board.
- * 
- * @author <put your name here>
+ *
+ * @author Qiwen Zhu
  */
 
 public class Coord implements Comparable<Coord> {
-  /**
-   *  The upper left corner of the board.
-   */
-  public static Coord ORIGIN = new Coord(0, 0);
-  
-  private int x, y;
+    /**
+     *  The upper left corner of the board.
+     */
+    public static Coord ORIGIN = new Coord(0, 0);
 
-  /**
-   * Constructs a new Coord that is a copy of the given Coord.
-   */
-  public Coord(Coord coord) {
-    this(coord.x, coord.y);
-  }
+    private int x, y;
 
-  /**
-   * Constructs a new Coord representing (x,y).
-   */
-  public Coord(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  /**
-   * Returns the Coord that is directly above (i.e., north of) this one.
-   */
-  public Coord up() {
-    return new Coord(x, y - 1);
-  }
-
-  /**
-   * Returns the Coord that is directly below (i.e., south of) this one.
-   */
-  public Coord down() {
-    return new Coord(x, y + 1);
-  }
-
-  /**
-   * Returns the Coord that is immediately to the left (i.e., west)
-   * of this one.
-   */
-  public Coord left() {
-    return new Coord(x - 1, y);
-  }
-
-  /**
-   * Returns the Coord that is immediately to the right (i.e., east)
-   * of this one.
-   */
-  public Coord right() {
-    return new Coord(x + 1, y);
-  }
-
-  /**
-   * TODO
-   * 
-   * Returns true iff this Coord is visible on a board of the given size.
-   */
-  public boolean onBoard(int size) {
-    return true;
-  }
-
-  /**
-   * TODO
-   * 
-   * Returns a list of the immediate board coordinates of this Coord's north,
-   * south, east, and west neighbors.
-   */
-  public List<Coord> neighbors(int size) {
-    return null;
-  }
-
-  /**
-   * Returns true iff the (x,y)-coordinates of the given object match this Coord's
-   * (x,y)-coordinates.
-   */
-  public boolean equals(Object obj) {
-    if (obj instanceof Coord) {
-      Coord that = (Coord) obj;
-      return that.x == this.x && that.y == this.y;
+    /**
+     * Constructs a new Coord that is a copy of the given Coord.
+     */
+    public Coord(Coord coord) {
+        this(coord.x, coord.y);
     }
-    return false;
-  }
 
-  /**
-   * Coords are ordered first on the x-coordinate and then on the y-coordinate.
-   */
-  public int compareTo(Coord that) {
-    if (this.x == that.x)
-      return this.y - that.y;
-    return this.x - that.x;
-  }
+    /**
+     * Constructs a new Coord representing (x,y).
+     */
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  /**
-   * Returns the x-coordinate.
-   */
-  public int getX() {
-    return x;
-  }
+    /**
+     * Returns the Coord that is directly above (i.e., north of) this one.
+     */
+    public Coord up()
+    {
+        return new Coord(x, y - 1);
+    }
 
-  /**
-   * Returns the y-coordinate.
-   */
-  public int getY() {
-    return y;
-  }
+    /**
+     * Returns the Coord that is directly below (i.e., south of) this one.
+     */
+    public Coord down()
+    {
+        return new Coord(x, y + 1);
+    }
 
-  /**
-   * TODO
-   * 
-   * Pre-hashing: pack this coordinate into an int, so that the key space is
-   * as uniformly distributed among the range of integers as possible.
-   */
-  public int hashCode() {
-    return 0;
-  }
+    /**
+     * Returns the Coord that is immediately to the left (i.e., west)
+     * of this one.
+     */
+    public Coord left()
+    {
+        return new Coord(x - 1, y);
+    }
 
-  /**
-   * Returns this Coord as a string of the form (x, y).
-   */
-  public String toString() {
-    return "(" + x + ", " + y + ")"; 
-  }
-  
-  /**
-   * Simple testing.
-   */
-  public static void main(String... args) {
-    Coord someCoord = new Coord(2, 1);
-    System.out.println("someCoord = " + someCoord);
-    System.out.println("someCoord.hashCode() = " + someCoord.hashCode());
-    System.out.println("someCoord.onBoard(4) = " + someCoord.onBoard(4));
-    System.out.println("neighbors on a 3x3 board = " + someCoord.neighbors(3));
-    System.out.println("neighbors on a 4x4 board = " + someCoord.neighbors(4));   
-    System.out.println();
-    
-    someCoord = ORIGIN;
-    System.out.println("someCoord = " + someCoord);
-    System.out.println("someCoord.hashCode() = " + someCoord.hashCode());
-    System.out.println("someCoord.onBoard(3) = " + someCoord.onBoard(3));
-    System.out.println("neighbors on a 3x3 board = " + someCoord.neighbors(3));
-    System.out.println("neighbors on a 1x1 board = " + someCoord.neighbors(1));   
-    System.out.println();
+    /**
+     * Returns the Coord that is immediately to the right (i.e., east)
+     * of this one.
+     */
+    public Coord right()
+    {
+        return new Coord(x + 1, y);
+    }
 
-    someCoord = new Coord(5, 5);
-    System.out.println("someCoord = " + someCoord);
-    System.out.println("someCoord.hashCode() = " + someCoord.hashCode());
-    System.out.println("someCoord.onBoard(5) = " + someCoord.onBoard(5));
-    System.out.println("neighbors on a 3x3 board = " + someCoord.neighbors(3));
-    System.out.println("neighbors on a 6x6 board = " + someCoord.neighbors(6));
-  }
+    /**
+     * TODO
+     *
+     * Returns true iff this Coord is visible on a board of the given size.
+     */
+    public boolean onBoard(int size)
+    {
+        if(getX() < 0 || getY() < 0)
+        {
+            return false;
+        }
+
+        if(getX() >= size || getY() >= size)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * TODO
+     *
+     * Returns a list of the immediate board coordinates of this Coord's north,
+     * south, east, and west neighbors.
+     */
+    public List<Coord> neighbors(int size)
+    {
+        List<Coord> neighbors = new ArrayList<>(4);
+        if(this.up().onBoard(size))
+        {
+            neighbors.add(this.up());
+        }
+
+        if(this.right().onBoard(size))
+        {
+            neighbors.add(this.right());
+        }
+
+        if(this.down().onBoard(size))
+        {
+            neighbors.add(this.down());
+        }
+
+        if(this.left().onBoard(size))
+        {
+            neighbors.add(this.left());
+        }
+
+        return neighbors;
+    }
+
+    /**
+     * Returns true iff the (x,y)-coordinates of the given object match this Coord's
+     * (x,y)-coordinates.
+     */
+    public boolean equals(Object obj) {
+        if (obj instanceof Coord) {
+            Coord that = (Coord) obj;
+            return that.x == this.x && that.y == this.y;
+        }
+        return false;
+    }
+
+    /**
+     * Coords are ordered first on the x-coordinate and then on the y-coordinate.
+     */
+    public int compareTo(Coord that) {
+        if (this.x == that.x)
+            return this.y - that.y;
+        return this.x - that.x;
+    }
+
+    /**
+     * Returns the x-coordinate.
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y-coordinate.
+     */
+    public int getY() {
+        return y;
+    }
+
+
+    /**
+     * Pre-hashing: pack this coordinate into an int, so that the key space is
+     * as uniformly distributed among the range of integers as possible.
+     *
+     * *********
+     * This piece of code is inspired by the source code in java.awt.geom.Point2D
+     * *********
+     * @return int
+     */
+    public int hashCode()
+    {
+        long bits = java.lang.Double.doubleToLongBits(getX());
+        bits ^= java.lang.Double.doubleToLongBits(getY()) * 31;
+        return (((int) bits) ^ ((int) (bits >> 31)));
+    }
+
+    /**
+     * Returns this Coord as a string of the form (x, y).
+     */
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Simple testing.
+     */
+    public static void main(String... args) {
+        Coord someCoord = new Coord(2, 1);
+        System.out.println("someCoord = " + someCoord);
+        System.out.println("someCoord.hashCode() = " + someCoord.hashCode());
+        System.out.println("someCoord.onBoard(4) = " + someCoord.onBoard(4));
+        System.out.println("neighbors on a 3x3 board = " + someCoord.neighbors(3));
+        System.out.println("neighbors on a 4x4 board = " + someCoord.neighbors(4));
+        System.out.println();
+
+        someCoord = ORIGIN;
+        System.out.println("someCoord = " + someCoord);
+        System.out.println("someCoord.hashCode() = " + someCoord.hashCode());
+        System.out.println("someCoord.onBoard(3) = " + someCoord.onBoard(3));
+        System.out.println("neighbors on a 3x3 board = " + someCoord.neighbors(3));
+        System.out.println("neighbors on a 1x1 board = " + someCoord.neighbors(1));
+        System.out.println();
+
+        someCoord = new Coord(5, 5);
+        System.out.println("someCoord = " + someCoord);
+        System.out.println("someCoord.hashCode() = " + someCoord.hashCode());
+        System.out.println("someCoord.onBoard(5) = " + someCoord.onBoard(5));
+        System.out.println("neighbors on a 3x3 board = " + someCoord.neighbors(3));
+        System.out.println("neighbors on a 6x6 board = " + someCoord.neighbors(6));
+    }
 }
